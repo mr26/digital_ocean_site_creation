53f4753fc685aa1b56c544d24a63cce83783b5e2
Mehdi Rahmani <34954226+mr26@users.noreply.github.com>
2020-04-07 09:50:04 -0400

    Update print-droplets.py

--- print-droplets.py
+++ print-droplets.py
@@ -7,7 +7,7 @@
 
 import digitalocean
 
-manager = digitalocean.Manager(token='38843b913b1b1c864bacc6c5959a0654e72349bb89f2728d9750e38ca9ef2188')
+manager = digitalocean.Manager(token='')
 
 
 for i in manager.get_all_droplets():
